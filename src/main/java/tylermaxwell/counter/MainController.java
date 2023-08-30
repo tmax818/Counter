d4dbaef3eee53c7ea60f52aef93f7e76e9d847f6
tmax818 <tylermaxwell661@gmail.com>
2023-08-30 15:32:00 -0600

    update

--- src/main/java/tylermaxwell/counter/MainController.java
+++ src/main/java/tylermaxwell/counter/MainController.java
@@ -3,7 +3,6 @@ package tylermaxwell.counter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
 
